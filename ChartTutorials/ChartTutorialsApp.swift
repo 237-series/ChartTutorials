ea09d22727d87c405f55b98362f9e4d51b941771
237-series <237.silly.mistake@gmail.com>
2023-04-03 16:48:09 +0900

    Initial Commit

---
+++ ChartTutorials/ChartTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartTutorialsApp.swift
+//  ChartTutorials
+//
+//  Created by sglee237 on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct ChartTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
